515edefd53b92a5a356371996c75826afa890357
yintianyu <eecsyty@outlook.com>
2018-12-19 21:35:04 +0800

    refine Butterfly

--- src/main/scala/fftr4/Butterfly_R4.scala
+++ src/main/scala/fftr4/Butterfly_R4.scala
@@ -1,46 +1,27 @@
-package fft
+package fftr4
+
 import chisel3._
 import chisel3.core.FixedPoint
-import chisel3.internal.firrtl._
-import Const._
-import chisel3.util._
 //import dsptools.numbers
 
 class Butterfly_R4 extends Module {
     val io = IO(new Bundle {
-//        val x1 = Input(new Complex(FixedPoint(DATAWIDTH.W, 5.BP), FixedPoint(DATAWIDTH.W, 5.BP)))
-//        val x2 = Input(new Complex(FixedPoint(DATAWIDTH.W, 5.BP), FixedPoint(DATAWIDTH.W, 5.BP)))
-//        val x3 = Input(new Complex(FixedPoint(DATAWIDTH.W, 5.BP), FixedPoint(DATAWIDTH.W, 5.BP)))
-//        val x4 = Input(new Complex(FixedPoint(DATAWIDTH.W, 5.BP), FixedPoint(DATAWIDTH.W, 5.BP)))
-//        val y1 = Output(new Complex(FixedPoint(DATAWIDTH.W, 5.BP), FixedPoint(DATAWIDTH.W, 5.BP)))
-//        val y2 = Output(new Complex(FixedPoint(DATAWIDTH.W, 5.BP), FixedPoint(DATAWIDTH.W, 5.BP)))
-//        val y3 = Output(new Complex(FixedPoint(DATAWIDTH.W, 5.BP), FixedPoint(DATAWIDTH.W, 5.BP)))
-//        val y4 = Output(new Complex(FixedPoint(DATAWIDTH.W, 5.BP), FixedPoint(DATAWIDTH.W, 5.BP)))
-        val x1 = Input(FixedPoint(10.W, 2.BP))
-        val x2 = Input(FixedPoint(10.W, 2.BP))
-        val x3 = Input(FixedPoint(10.W, 2.BP))
-        val x4 = Input(FixedPoint(10.W, 2.BP))
-        val y1 = Output(FixedPoint(10.W, 2.BP))
-        val y2 = Output(FixedPoint(10.W, 2.BP))
-        val y3 = Output(FixedPoint(10.W, 2.BP))
-        val y4 = Output(FixedPoint(10.W, 2.BP))
+        val xs = Input(Vec(4, new FFTComplex))
+        val ys = Output(Vec(4, new FFTComplex))
+//        val xs = VecInit(Seq.fill(4)(Input(new FFTComplex())))
+//        val ys = VecInit(Seq.fill(4)(Output(new FFTComplex())))
     })
-//    val minus_j = new Complex(FixedPoint(0, DATAWIDTH.W, 5.BP), FixedPoint(-1, DATAWIDTH.W, 5.BP))
-//    val plus_j = new Complex(FixedPoint(0, DATAWIDTH.W, 5.BP), FixedPoint(1, DATAWIDTH.W, 5.BP))
-    val y1 = io.x1 + io.x2 + io.x3 + io.x4
-//    val y2 = io.x1 + io.x2 * minus_j - io.x3 + io.x4 * plus_j
-//    val y3 = io.x1 - io.x2 + io.x3 - io.x4
-//    val y4 = io.x1 + io.x2 * plus_j - io.x3 + io.x4 * minus_j
-    io.y1 := y1
-    io.y2 := y1
-    io.y3 := y1
-    io.y4 := y1
-//    io.y1.imag := y1.imag
-//    io.y1.real := y1.real
-//    io.y2.imag := y2.imag
-//    io.y2.real := y2.real
-//    io.y3.imag := y3.imag
-//    io.y3.real := y3.real
-//    io.y4.imag := y4.imag
-//    io.y4.real := y4.real
+    val intermediate = Wire(new UnitPort)
+    intermediate(0) := io.xs(0) + io.xs(2)
+    intermediate(1) := io.xs(1) + io.xs(3)
+    intermediate(2) := io.xs(0) - io.xs(2)
+    intermediate(3) := io.xs(1) - io.xs(3)
+    io.ys(0) := intermediate(0) + intermediate(1)
+    io.ys(1) := intermediate(0) - intermediate(1)
+    io.ys(2) := intermediate(2) - intermediate(3).xj
+    io.ys(3) := intermediate(2) + intermediate(3).xj
+//    io.ys(0) := io.xs(0) + io.xs(1) + io.xs(2) + io.xs(3)
+//    io.ys(1) := io.xs(0) - io.xs(1) + io.xs(2) - io.xs(3)
+//    io.ys(2) := io.xs(0) - io.xs(1).xj - io.xs(2) + io.xs(3).xj
+//    io.ys(3) := io.xs(0) + io.xs(1).xj - io.xs(2) - io.xs(3).xj
 }
--- src/main/scala/fftr4/Controller.scala
+++ src/main/scala/fftr4/Controller.scala
@@ -1,1 +1,32 @@
 
+package fftr4
+import chisel3._
+import chisel3.util._
+
+class Controller extends Module{
+    val io = IO(new Bundle{
+        val valid = Input(Bool())
+        val stage0_valid = Output(Bool())
+        val stage1_sw_sel = Output(UInt(2.W))
+        val stage1_valid = Output(Bool())
+        val stage2_sw_sel = Output(UInt(2.W))
+        val reorder_valid = Output(Bool())
+        val output_valid = Output(Bool())
+        val cnt = Output(UInt(32.W))
+    })
+    val cnt = RegInit(0.U(32.W))    // TODO: Specify the width of cnt
+    when(io.valid === true.B){
+        cnt := cnt + 1.U
+    }otherwise {
+        cnt := 0.U
+    }
+    io.stage0_valid := (cnt >= 48.U)
+    io.stage1_sw_sel := cnt(3, 2)
+    io.stage1_valid := (cnt >= 60.U)
+    io.stage2_sw_sel := cnt(1,0)
+    io.reorder_valid := (cnt >= 63.U) && (cnt < (63+16).U)
+    io.output_valid := (cnt >= 127.U)
+    io.cnt := cnt
+
+//    printf("cnt = %d\n", cnt)
+}
